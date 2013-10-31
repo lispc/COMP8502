afa3a195c0e12cb72e32cdbdf14e306195617055
Tomas Tauber <tomtau@hku.hk>
2013-10-31 13:49:16 +0800

    Analysing folders

--- feature_extract/extract.py
+++ feature_extract/extract.py
@@ -5,8 +5,9 @@ PATH_INSTALL = "../../androguard"
 
 sys.path.append(PATH_INSTALL)
 
-import arff
+import arff, os
 import androlyze
+from os import listdir
 
 from androlyze import AnalyzeAPK
 
@@ -27,17 +28,34 @@ from androguard.core.bytecodes.api_permissions import DVM_PERMISSIONS_BY_PERMISS
 
 ALL_PERMISSIONS = sorted(DVM_PERMISSIONS_BY_PERMISSION.keys())
 
+def get_app_data(app, malware):
+	app_perms = set(map(lambda x: x.rsplit('.', 1)[-1], app.get_permissions()))
+	data = [malware]
+	for perm in ALL_PERMISSIONS:
+		if perm in app_perms:
+			data.append(True)
+		else:
+			data.append(False)
+	return data
+
 if __name__ == "__main__" :
 	if len(sys.argv) < 3:
-		print("python extract.py <path-to-apk> <output>")
+		print("python extract.py <apps-folder> <malware-folder> <output>")
 	else:
-		a, d, dx = AnalyzeAPK(sys.argv[1])
-		app_perms = set(map(lambda x: x.rsplit('.', 1)[-1], a.get_permissions()))
+		if len(sys.argv) == 4:
+			output = argv[3]
+		else:
+			output = "training.arff"
+		apps = listdir(sys.argv[1])
+		malware = listdir(sys.argv[2])
 		data = []
-		for perm in ALL_PERMISSIONS:
-			if perm in app_perms:
-				data.append(True)
-			else:
-				data.append(False)
-
-		arff.dump(sys.argv[2], [data], relation="APK", names=ALL_PERMISSIONS)
+		for app in apps:
+			print(app)
+			a, d, dx = AnalyzeAPK(os.path.join(sys.argv[1], app))
+			data.append(get_app_data(a, False))
+		for mal in malware:
+			print(mal)
+			a, d, dx = AnalyzeAPK(os.path.join(sys.argv[2], mal))
+			data.append(get_app_data(a, True))
+			
+		arff.dump(output, data, relation="APK", names=["MALWARE"] + ALL_PERMISSIONS)
