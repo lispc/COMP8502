76a6ed89b7f6059c5164baade65a345e19f72735
lispc <mycinbrin@gmail.com>
2013-11-23 00:24:35 +0800

    change to sbt

--- predict/test_scala.sh
+++ predict/test_scala.sh
@@ -1,4 +1,2 @@
-#scala -cp /usr/share/java/weka.jar validate_and_predict.scala test.arff weka.classifiers.bayes.NaiveBayes ""| tee scala.log
-#java -cp .:/usr/share/java/weka.jar:/usr/share/scala/lib/scala-library.jar Test test.arff weka.classifiers.bayes.NaiveBayes ""| tee scala.log
-#java -cp ./final.jar Test ../data/test.arff weka.classifiers.bayes.NaiveBayes ""| tee scala.log
-java -cp final.jar Test ../data/training.arff ../data/test.arff weka.classifiers.functions.SMO "-C 1.0 -L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1 -K \"weka.classifiers.functions.supportVector.PolyKernel -C 250007 -E 1.0\""
+sbt one-jar
+java -jar target/scala-2.10/predictor_2.10-0.0-one-jar.jar ../data/training.arff ../data/test.arff weka.classifiers.functions.SMO "-C 1.0 -L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1 -K \"weka.classifiers.functions.supportVector.PolyKernel -C 250007 -E 1.0\""
